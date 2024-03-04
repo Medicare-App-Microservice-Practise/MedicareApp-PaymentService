36e356f4befd74339417de2dee16105a0250a8fc
arvend <arvendrajan100@gmail.com>
2024-03-04 15:35:15 +0800

    initial commit

---
+++ src/main/java/com/medicare/paymentservice/MedicarePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicarePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicarePaymentServiceApplication.class, args);
+	}
+
+}
